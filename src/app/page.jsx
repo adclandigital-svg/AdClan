3cb253f7ab592381760eb4954d7bc94083f81bbc
adclandigital-svg <adclandigital@gmail.com>
2025-12-24 15:58:38 +0530

    new3

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,7 +6,7 @@ const Hero = dynamic(() => import("@/components/home/Hero"), { ssr: false });
 const LoadingScreen = dynamic(()=>import("@/components/home/loading/LoadingScreen"))
 
 const HomeSecond = dynamic(() => import("@/components/home/HomeSecond"), { ssr: false });
-const HomeThird = dynamic(() => import("@/components/home/HomeThird"), { ssr: false });
+// const HomeThird = dynamic(() => import("@/components/home/HomeThird"), { ssr: false });
 const HomeFourth = dynamic(()=>import("@/components/home/HomeFourth"), { ssr: false })
 const HomeFifth = dynamic(()=>import("@/components/home/HomeFifth"), { ssr: false })
 const HomeSix = dynamic(()=>import("@/components/home/HomeSix"), { ssr: false })
@@ -18,7 +18,7 @@ export default function Home() {
       <Suspense fallback={<div>Loading Hero...</div>}>
         <Hero />
         <HomeSecond/>
-        <HomeThird/>
+        {/* <HomeThird/> */}
         <HomeFourth/>
         <HomeFifth/> 
         <HomeSix/>
