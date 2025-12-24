c00a6cd46f5fed1d8a343aef295ae1e64ff7f171
adclandigital-svg <adclandigital@gmail.com>
2025-12-24 16:04:53 +0530

    new6

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -5,7 +5,7 @@ import { Suspense } from "react";
 const Hero = dynamic(() => import("@/components/home/Hero"), { ssr: false });
 const LoadingScreen = dynamic(()=>import("@/components/home/loading/LoadingScreen"))
 
-// const HomeSecond = dynamic(() => import("@/components/home/HomeSecond"), { ssr: false });
+const HomeSecond = dynamic(() => import("@/components/home/HomeSecond"), { ssr: false });
 const HomeThird = dynamic(() => import("@/components/home/HomeThird"), { ssr: false });
 const HomeFourth = dynamic(()=>import("@/components/home/HomeFourth"), { ssr: false })
 const HomeFifth = dynamic(()=>import("@/components/home/HomeFifth"), { ssr: false })
@@ -14,7 +14,7 @@ const HomeSix = dynamic(()=>import("@/components/home/HomeSix"), { ssr: false })
 export default function Home() {
   return (
     <>
-    {/* <LoadingScreen/> */}
+    <LoadingScreen/>
       <Suspense fallback={<div>Loading Hero...</div>}>
         <Hero />
         {/* <HomeSecond/> */}
