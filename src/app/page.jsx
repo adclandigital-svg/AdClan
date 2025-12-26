054bc6d7254a755a81f437457e69ce0fdd6a64d2
adclandigital-svg <adclandigital@gmail.com>
2025-12-26 18:12:31 +0530

    wer23

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,58 +1,47 @@
 "use client";
-// import dynamic from "next/dynamic";
-// import { Suspense } from "react";
+import dynamic from "next/dynamic";
+import { Suspense } from "react";
 
-// const Hero = dynamic(() => import("@/components/home/Hero"), { ssr: false });
+const Hero = dynamic(() => import("@/components/home/Hero"), { ssr: false });
 
-// const HomeSecond = dynamic(() => import("@/components/home/HomeSecond"), {
-//   ssr: false,
-// });
-// const HomeThird = dynamic(() => import("@/components/home/HomeThird"), {
-//   ssr: false,
-// });
-// const HomeFourth = dynamic(() => import("@/components/home/HomeFourth"), {
-//   ssr: false,
-// });
-// const HomeFifth = dynamic(() => import("@/components/home/HomeFifth"), {
-//   ssr: false,
-// });
-// const HomeSix = dynamic(() => import("@/components/home/HomeSix"), {
-//   ssr: false,
-// });
-// const HomeSeven = dynamic(() => import("@/components/home/HomeSeven"), {
-//   ssr: false,
-// });
-// const HomeEight = dynamic(() => import("@/components/home/HomeEight"), {
-//   ssr: false,
-// });
+const HomeSecond = dynamic(() => import("@/components/home/HomeSecond"), {
+  ssr: false,
+});
+const HomeThird = dynamic(() => import("@/components/home/HomeThird"), {
+  ssr: false,
+});
+const HomeFourth = dynamic(() => import("@/components/home/HomeFourth"), {
+  ssr: false,
+});
+const HomeFifth = dynamic(() => import("@/components/home/HomeFifth"), {
+  ssr: false,
+});
+const HomeSix = dynamic(() => import("@/components/home/HomeSix"), {
+  ssr: false,
+});
+const HomeSeven = dynamic(() => import("@/components/home/HomeSeven"), {
+  ssr: false,
+});
+const HomeEight = dynamic(() => import("@/components/home/HomeEight"), {
+  ssr: false,
+});
 
 export default function Home() {
   return (
-    // <>
-    //   <Suspense fallback={<div>Loading Hero...</div>}>
-    //     <Hero />
-    //     <HomeSecond />
-    //     <HomeSeven />
-
-    //     <HomeThird />
-    //     <HomeFourth />
-    //     <HomeFifth />
-    //     <HomeSix />
-    //     <HomeEight />
-    //   </Suspense>
-    // </>
-
     <>
-      {/* <Hero />
-      <HomeSecond />
-      <HomeSeven />
-      <Suspense fallback={null}>
+      <Suspense fallback={<div>Loading Hero...</div>}>
+        <Hero />
+        <HomeSecond />
+        <HomeSeven />
+
         <HomeThird />
+        <HomeFourth />
+        <HomeFifth />
+        <HomeSix />
+        <HomeEight />
       </Suspense>
-      <HomeFourth />
-      <HomeFifth />
-      <HomeSix />
-      <HomeEight /> */}
     </>
+
+
   );
 }
