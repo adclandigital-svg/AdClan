cdd52df35138164d326a369c45b9c86e9cfce4fc
adclandigital-svg <adclandigital@gmail.com>
2026-02-07 11:22:03 +0530

    ch

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -22,7 +22,7 @@ export default function RootLayout({ children }) {
       </head>
       <body >
         <LenisProvider>
-          {/* <LoadingScreen /> */}
+          <LoadingScreen />
           <Navbar />
           {children}
           <Footer />
